cdc2ca5d529c09f6c5c13abe53d1d7d0b30e33dd
gupta.ishan.1510 <gupta.ishan.1510@gmail.com>
2024-04-13 13:44:58 +0530

    back

--- server/build/src/index.js
+++ server/build/src/index.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
@@ -11,9 +13,9 @@ const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
 app.get("/", (req, res) => {
-    res.send("hello world");
+  res.send("hello world");
 });
 app.use("/user-auth", user_auth_1.default);
 app.listen(PORT, () => {
-    console.log(`Server is up and running on PORT: ${PORT}`);
+  console.log(`Server is up and running on PORT: ${PORT}`);
 });
